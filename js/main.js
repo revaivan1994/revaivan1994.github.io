8c9e293cea97c6af6c4280220f8f591244693010
Ivan <92686306+revaivan1994@users.noreply.github.com>
2023-02-04 14:36:09 +0200

    second_comit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function(){
+    $('.slider__box').slick({
+        prevArrow: '<img src="images/arrow-left.png" alt="arrow-left" class="slider__arrow slider__arrow-left">',
+        nextArrow: '<img src="images/arrow-right.png" alt="arrow-right" class="slider__arrow slider__arrow-right">',
+        responsive: [
+            {
+              breakpoint: 481,
+              settings: {
+                arrows: false
+              }
+            }
+        ]
+    });
 
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('active');
+    });
 });
